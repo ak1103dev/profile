3a91c2e2fb9d717b58c3aaac64e8b8755325c76f
ak1103dev <ak.apichan1103@gmail.com>
2017-06-27 12:56:56 +0700

    :construction: change title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default () => (
-  <div>Click <Link href="/about"><a>this</a></Link> to read more</div>
+  <div>
+    <Head>
+      <title>Apichan Chaiyutthasat</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <div className="container">
+      <h1>Apichan Chaiyutthasat</h1>
+      <div>
+
+      </div>
+    </div>
+    <style jsx>{`
+      .container {
+        text-align: center;
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        background-color: #00b900;
+        color: #FFF;
+      }
+    `}</style>
+  </div>
 );
