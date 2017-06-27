eb26b1d00eaf3e127eeac5b2082f093cce839ba3
ak1103dev <ak.apichan1103@gmail.com>
2017-06-27 09:24:05 +0700

    :construction: test ci

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import Link from 'next/link';
 
 export default () => (
-  <div>Click <Link href="/about"><a>here</a></Link> to read more</div>
+  <div>Click <Link href="/about"><a>this</a></Link> to read more</div>
 );
