a6316a2de90c6ce388a3c299264755c857516989
ak1103dev <ak.apichan1103@gmail.com>
2017-06-30 00:00:54 +0700

    :art: change Head tag position

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 // import Link from 'next/link';
-import Head from 'next/head';
 import NavigationBar from '../components/NavigationBar';
 import Input from '../components/Input';
 
 export default () => (
   <div>
-    <Head>
-      <title>ChatToMe</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
     <div className="container">
       <NavigationBar />
       <Input />
@@ -19,11 +14,5 @@ export default () => (
         width: 100%;
       }
     `}</style>
-    {/* <style global jsx>{`
-      body {
-        background-color: #00b900;
-        color: #FFF;
-      }
-    `}</style>*/}
   </div>
 );
