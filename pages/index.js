2f79834fc3b8a05086bb6cef957ea119443e2c13
ak1103dev <ak.apichan1103@gmail.com>
2017-06-30 00:58:50 +0700

    :construction: init messages

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import Link from 'next/link';
 import NavigationBar from '../components/NavigationBar';
 import Input from '../components/Input';
+import Messages from '../components/Messages';
 
-export default () => (
-  <div>
-    <div className="container">
-      <NavigationBar />
-      <Input />
-    </div>
-    <style jsx>{`
-      .container {
-        width: 100%;
-      }
-    `}</style>
-  </div>
-);
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [{
+        message: 'hello',
+        isOwner: true,
+      }],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <NavigationBar />
+          <Messages messages={this.state.messages} />
+          <Input />
+        </div>
+        <style jsx>{`
+          .container {
+            width: 100%;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
